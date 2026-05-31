bcaab6b79b632a2336f5826605bc1559d92da98e
ruheHowest <herreman.rune@gmail.com>
2026-05-31 11:33:21 +0200

    give all fake users stable fixed UUIDs

--- app/src/main/java/be/runeherreman/zuyp/data/fake/data/FakeUsers.kt
+++ app/src/main/java/be/runeherreman/zuyp/data/fake/data/FakeUsers.kt
@@ -6,7 +6,7 @@ import java.util.UUID
 
 object FakeUsers {
     val userJan = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000001"),
         "Jan Ketelman",
         LocalDate.of(2006, 8, 20),
         "jan.ketelman@gmail.com"
@@ -18,343 +18,343 @@ object FakeUsers {
         "koen.koreman@gmail.com"
     )
     val userLotte = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000002"),
         "Lotte Van Damme",
         LocalDate.of(2004, 3, 14),
         "lotte.vandamme@gmail.com"
     )
     val userMilan = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000003"),
         "Milan De Smet",
         LocalDate.of(2001, 11, 5),
         "milan.desmet@gmail.com"
     )
     val userNora = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000004"),
         "Nora Peeters",
         LocalDate.of(2005, 1, 27),
         "nora.peeters@gmail.com"
     )
     val userBram = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000005"),
         "Bram Verdonck",
         LocalDate.of(2003, 6, 9),
         "bram.verdonck@gmail.com"
     )
     val userElise = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000006"),
         "Elise Maes",
         LocalDate.of(2004, 12, 1),
         "elise.maes@gmail.com"
     )
     val userTibo = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000007"),
         "Tibo Claes",
         LocalDate.of(2002, 4, 18),
         "tibo.claes@gmail.com"
     )
     val userMila = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000008"),
         "Mila Janssens",
         LocalDate.of(2004, 2, 11),
         "mila.janssens@gmail.com"
     )
     val userRuben = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000009"),
         "Ruben Claeys",
         LocalDate.of(2002, 5, 7),
         "ruben.claeys@gmail.com"
     )
     val userNoor = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000010"),
         "Noor De Wilde",
         LocalDate.of(2005, 10, 22),
         "noor.dewilde@gmail.com"
     )
     val userSanne = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000011"),
         "Sanne Desmet",
         LocalDate.of(2004, 7, 14),
         "sanne.desmet@gmail.com"
     )
     val userDaan = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000012"),
         "Daan Heylen",
         LocalDate.of(2003, 9, 3),
         "daan.heylen@gmail.com"
     )
     val userLuna = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000013"),
         "Luna Smets",
         LocalDate.of(2005, 5, 19),
         "luna.smets@gmail.com"
     )
     val userMaxim = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000014"),
         "Maxim Vercammen",
         LocalDate.of(2002, 11, 11),
         "maxim.vercammen@gmail.com"
     )
     val userAva = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000015"),
         "Ava Vermeersch",
         LocalDate.of(2004, 8, 26),
         "ava.vermeersch@gmail.com"
     )
     val userFelix = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000016"),
         "Felix Vandebrouck",
         LocalDate.of(2003, 1, 8),
         "felix.vandebrouck@gmail.com"
     )
     val userZoe = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000017"),
         "Zoe Vandersmissen",
         LocalDate.of(2005, 3, 22),
         "zoe.vandersmissen@gmail.com"
     )
     val userJoren = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000018"),
         "Joren Verlinden",
         LocalDate.of(2002, 10, 5),
         "joren.verlinden@gmail.com"
     )
     val userIsabella = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000019"),
         "Isabella Verhaeghe",
         LocalDate.of(2004, 6, 17),
         "isabella.verhaeghe@gmail.com"
     )
     val userSebastian = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000020"),
         "Sebastian Lampe",
         LocalDate.of(2001, 12, 30),
         "sebastian.lampe@gmail.com"
     )
     val userNatasja = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000021"),
         "Natasja Vos",
         LocalDate.of(2004, 4, 11),
         "natasja.vos@gmail.com"
     )
     val userThijs = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000022"),
         "Thijs Daemen",
         LocalDate.of(2003, 7, 28),
         "thijs.daemen@gmail.com"
     )
     val userStéphanie = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000023"),
         "Stéphanie Devos",
         LocalDate.of(2004, 2, 9),
         "stephanie.devos@gmail.com"
     )
     val userMarkus = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000024"),
         "Markus Peeters",
         LocalDate.of(2002, 8, 14),
         "markus.peeters@gmail.com"
     )
     val userCamille = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000025"),
         "Camille Ronsse",
         LocalDate.of(2005, 9, 2),
         "camille.ronsse@gmail.com"
     )
     val userVictoria = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000026"),
         "Victoria Leemans",
         LocalDate.of(2003, 5, 25),
         "victoria.leemans@gmail.com"
     )
     val userDieter = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000027"),
         "Dieter Wagemans",
         LocalDate.of(2002, 3, 16),
         "dieter.wagemans@gmail.com"
     )
     val userLea = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000028"),
         "Lea Snoeck",
         LocalDate.of(2004, 11, 7),
         "lea.snoeck@gmail.com"
     )
     val userRyan = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000029"),
         "Ryan Debrouwer",
         LocalDate.of(2003, 6, 20),
         "ryan.debrouwer@gmail.com"
     )
     val userSophie = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000030"),
         "Sophie Delvaux",
         LocalDate.of(2005, 1, 12),
         "sophie.delvaux@gmail.com"
     )
     val userQuentin = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000031"),
         "Quentin Claes",
         LocalDate.of(2002, 9, 4),
         "quentin.claes@gmail.com"
     )
     val userEva = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000032"),
         "Eva Vanheule",
         LocalDate.of(2004, 10, 19),
         "eva.vanheule@gmail.com"
     )
     val userLars = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000033"),
         "Lars Bosmans",
         LocalDate.of(2003, 2, 8),
         "lars.bosmans@gmail.com"
     )
     val userAnna = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000034"),
         "Anna Vanzeir",
         LocalDate.of(2004, 7, 31),
         "anna.vanzeir@gmail.com"
     )
     val userTom = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000035"),
         "Tom Baert",
         LocalDate.of(2002, 4, 22),
         "tom.baert@gmail.com"
     )
     val userEmilie = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000036"),
         "Emilie Briers",
         LocalDate.of(2005, 8, 6),
         "emilie.briers@gmail.com"
     )
     val userPhilip = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000037"),
         "Philip Everard",
         LocalDate.of(2002, 6, 13),
         "philip.everard@gmail.com"
     )
     val userClaire = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000038"),
         "Claire Genon",
         LocalDate.of(2004, 9, 29),
         "claire.genon@gmail.com"
     )
     val userSven = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000039"),
         "Sven Guelinckx",
         LocalDate.of(2001, 12, 11),
         "sven.guelinckx@gmail.com"
     )
     val userBeat = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000040"),
         "Beat Greyling",
         LocalDate.of(2003, 8, 24),
         "beat.greyling@gmail.com"
     )
     val userLena = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000041"),
         "Lena Goyvaerts",
         LocalDate.of(2005, 4, 5),
         "lena.goyvaerts@gmail.com"
     )
     val userJulian = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000042"),
         "Julian Groote",
         LocalDate.of(2003, 11, 18),
         "julian.groote@gmail.com"
     )
     val userSienna = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000043"),
         "Sienna Gregoor",
         LocalDate.of(2004, 3, 27),
         "sienna.gregoor@gmail.com"
     )
     val userAlex = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000044"),
         "Alex Grill",
         LocalDate.of(2002, 10, 9),
         "alex.grill@gmail.com"
     )
     val userOliver = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000045"),
         "Oliver Hanssens",
         LocalDate.of(2005, 2, 14),
         "oliver.hanssens@gmail.com"
     )
     val userMaya = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000046"),
         "Maya Vandenbroeck",
         LocalDate.of(2003, 12, 3),
         "maya.vandenbroeck@gmail.com"
     )
     val userLuc = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000047"),
         "Luc Vanhove",
         LocalDate.of(2002, 1, 21),
         "luc.vanhove@gmail.com"
     )
     val userAnne = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000048"),
         "Anne Verheggen",
         LocalDate.of(2004, 5, 10),
         "anne.verheggen@gmail.com"
     )
     val userDavid = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000049"),
         "David Verhoeven",
         LocalDate.of(2003, 9, 7),
         "david.verhoeven@gmail.com"
     )
     val userFlorence = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000050"),
         "Florence Veron",
         LocalDate.of(2005, 7, 23),
         "florence.veron@gmail.com"
     )
     val userNico = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000051"),
         "Nico Verpieren",
         LocalDate.of(2002, 11, 2),
         "nico.verpieren@gmail.com"
     )
     val userGrace = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000052"),
         "Grace Veuger",
         LocalDate.of(2004, 10, 15),
         "grace.veuger@gmail.com"
     )
     val userChris = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000053"),
         "Chris Vierde",
         LocalDate.of(2003, 4, 28),
         "chris.vierde@gmail.com"
     )
     val userMaria = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000054"),
         "Maria Villard",
         LocalDate.of(2005, 6, 12),
         "maria.villard@gmail.com"
     )
     val userSteve = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000055"),
         "Steve Vivier",
         LocalDate.of(2001, 8, 19),
         "steve.vivier@gmail.com"
     )
     val userJessica = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000056"),
         "Jessica Voegele",
         LocalDate.of(2004, 1, 6),
         "jessica.voegele@gmail.com"
     )
     val userPaul = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000057"),
         "Paul Vogel",
         LocalDate.of(2003, 3, 17),
         "paul.vogel@gmail.com"
     )
     val userRosa = User(
-        UUID.randomUUID(),
+        UUID.fromString("20000000-0000-0000-0000-000000000058"),
         "Rosa Voigt",
         LocalDate.of(2005, 11, 24),
         "rosa.voigt@gmail.com"
@@ -372,4 +372,3 @@ object FakeUsers {
         userMaria, userSteve, userJessica, userPaul, userRosa
     )
 }
-
