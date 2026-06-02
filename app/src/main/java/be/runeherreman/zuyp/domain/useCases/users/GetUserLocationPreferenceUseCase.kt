116d9fc0df6f7beaebafaf31537569d2ab168ecc
ruheHowest <herreman.rune@gmail.com>
2026-06-02 12:50:08 +0200

    new use cases

---
+++ app/src/main/java/be/runeherreman/zuyp/domain/useCases/users/GetUserLocationPreferenceUseCase.kt
@@ -0,0 +1,11 @@
+package be.runeherreman.zuyp.domain.useCases.users
+
+import be.runeherreman.zuyp.data.local.datastore.SettingsDataStore
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetUserLocationPreferenceUseCase @Inject constructor(
+    private val settingsDataStore: SettingsDataStore
+) {
+    operator fun invoke(): Flow<Boolean> = settingsDataStore.locationEnabled
+}
