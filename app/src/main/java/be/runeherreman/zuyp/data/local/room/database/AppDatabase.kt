ef156d52e942212ae8a95dbf334f5c09c47339ef
ruheHowest <herreman.rune@gmail.com>
2026-06-03 15:42:25 +0200

    Add image file paths to manifest

--- app/src/main/java/be/runeherreman/zuyp/data/local/room/database/AppDatabase.kt
+++ app/src/main/java/be/runeherreman/zuyp/data/local/room/database/AppDatabase.kt
@@ -3,12 +3,13 @@ package be.runeherreman.zuyp.data.local.room.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import be.runeherreman.zuyp.data.local.room.dao.ExpenseDao
 import be.runeherreman.zuyp.data.local.room.dao.GroupDao
 import be.runeherreman.zuyp.data.local.room.dao.HangoutDao
 import be.runeherreman.zuyp.data.local.room.dao.UserDao
 import be.runeherreman.zuyp.data.local.room.entity.expenses.ExpenseEntity
 import be.runeherreman.zuyp.data.local.room.entity.expenses.ExpenseShareEntity
-import be.runeherreman.zuyp.data.local.room.entity.expenses.ExpenseWithDetails
+import be.runeherreman.zuyp.data.local.room.entity.expenses.SettlementEntity
 import be.runeherreman.zuyp.data.local.room.entity.users.FriendshipEntity
 import be.runeherreman.zuyp.data.local.room.entity.groups.GroupEntity
 import be.runeherreman.zuyp.data.local.room.entity.groups.GroupUserMapping
@@ -22,8 +23,11 @@ import be.runeherreman.zuyp.data.local.room.entity.hangouts.HangoutUsersMapping
         UserEntity::class,
         HangoutUsersMapping::class,
         FriendshipEntity::class,
-        GroupEntity::class, GroupUserMapping::class,
-        ExpenseEntity::class, ExpenseShareEntity::class, ExpenseWithDetails::class
+        GroupEntity::class,
+        GroupUserMapping::class,
+        ExpenseEntity::class,
+        ExpenseShareEntity::class,
+        SettlementEntity::class
    ],
     version = 12,
     exportSchema = false
