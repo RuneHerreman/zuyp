b1e049c3aec747cec8383157da4318ed1edc9a30
ruheHowest <herreman.rune@gmail.com>
2026-06-04 15:53:20 +0200

    Add background location permissions

--- app/src/main/java/be/runeherreman/zuyp/ui/permissions/AppPermission.kt
+++ app/src/main/java/be/runeherreman/zuyp/ui/permissions/AppPermission.kt
@@ -7,6 +7,7 @@ enum class AppPermission {
     FLASHLIGHT,
     LOCATION,
     CAMERA,
+    BACKGROUND_LOCATION
 }
 
 fun AppPermission.toAndroidPermissions(): List<String> {
@@ -14,6 +15,7 @@ fun AppPermission.toAndroidPermissions(): List<String> {
         AppPermission.NOTIFICATION -> listOf(Manifest.permission.POST_NOTIFICATIONS)
         AppPermission.FLASHLIGHT -> listOf(Manifest.permission.CAMERA)
         AppPermission.LOCATION -> listOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+        AppPermission.BACKGROUND_LOCATION -> listOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
         AppPermission.CAMERA -> listOf(Manifest.permission.CAMERA)
     }
 }
