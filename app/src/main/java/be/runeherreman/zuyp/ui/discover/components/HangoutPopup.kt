9452a181802c12b8d33e5374b349d0ddf68d12c6
ruheHowest <herreman.rune@gmail.com>
2026-06-05 12:56:47 +0200

    imports fix

--- app/src/main/java/be/runeherreman/zuyp/ui/discover/components/HangoutPopup.kt
+++ app/src/main/java/be/runeherreman/zuyp/ui/discover/components/HangoutPopup.kt
@@ -1,5 +1,6 @@
 package be.runeherreman.zuyp.ui.discover.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -29,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import be.runeherreman.zuyp.data.local.room.entity.hangouts.AttendanceStatus
 import be.runeherreman.zuyp.domain.model.Hangout
