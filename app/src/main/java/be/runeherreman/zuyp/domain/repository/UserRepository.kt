91603183f2a7350a5a8efa86ee290f2b21d52b75
ruheHowest <herreman.rune@gmail.com>
2026-06-08 15:57:24 +0200

    Remove context from

--- app/src/main/java/be/runeherreman/zuyp/domain/repository/UserRepository.kt
+++ app/src/main/java/be/runeherreman/zuyp/domain/repository/UserRepository.kt
@@ -1,11 +1,12 @@
 package be.runeherreman.zuyp.domain.repository
 
 import be.runeherreman.zuyp.domain.model.User
+import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
 interface UserRepository {
     suspend fun getUserById(id: UUID): User?
-    suspend fun getAllUsers(): List<User>
+    fun getAllUsers(): Flow<List<User>>
     suspend fun areFriends(userId1: UUID, userId2: UUID): Boolean
     suspend fun editProfile(user: User)
     suspend fun getFriendsOfUser(userId: UUID): List<User>
--- app/src/main/java/be/runeherreman/zuyp/domain/usecases/users/GetAllUsersUseCase.kt
+++ app/src/main/java/be/runeherreman/zuyp/domain/usecases/users/GetAllUsersUseCase.kt
@@ -2,12 +2,11 @@ package be.runeherreman.zuyp.domain.usecases.users
 
 import be.runeherreman.zuyp.domain.model.User
 import be.runeherreman.zuyp.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetAllUsersUseCase @Inject constructor(
     private val userRepository: UserRepository
 ) {
-    suspend operator fun invoke(): List<User> {
-        return userRepository.getAllUsers()
-    }
+    operator fun invoke(): Flow<List<User>> = userRepository.getAllUsers()
 }
