13c8a2fb674d0c21854f9f3e43df805eadb3ce5c
ruheHowest <herreman.rune@gmail.com>
2026-06-08 15:53:25 +0200

    Add logging

--- app/src/main/java/be/runeherreman/zuyp/ZuypApplication.kt
+++ app/src/main/java/be/runeherreman/zuyp/ZuypApplication.kt
@@ -2,6 +2,7 @@ package be.runeherreman.zuyp
 
 import android.app.Application
 import android.content.Intent
+import android.util.Log
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import be.runeherreman.zuyp.data.local.room.database.DatabaseSeeder
@@ -30,8 +31,14 @@ class ZuypApplication : Application(), Configuration.Provider {
         NotificationHelper.createNotificationChannels(this)
         try {
             startForegroundService(Intent(this, MessagingService::class.java))
-        } catch (_: Exception) {
-            // startForegroundService is not allowed when launched from background (Android 12+)
+        } catch (e: IllegalStateException) {
+            Log.w(TAG, "MessagingService not started: app launched from background (${e.message})")
+        } catch (e: Exception) {
+            Log.e(TAG, "Failed to start MessagingService", e)
         }
+    }
+
+    companion object {
+        private const val TAG = "ZuypApplication"
     }
 }
